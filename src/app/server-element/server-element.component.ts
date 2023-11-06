07af02a28ea47f7e5eca2fdfc22ad484386102b7
Bakr Hawsawi <bakrhawsawi4@gmail.com>
2023-11-06 08:30:57 +0300

    commit

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ContentChild, ElementRef, AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, AfterContentInit {
   @Input() element: { type: string, name: string, content: string }
+  @ContentChild("contentParagrph", { static: true }) paragraph: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
+    console.log("onInit Called!");
+    console.log("content text paragrph:" + this.paragraph.nativeElement.textContent);
+
+  }
+
+  ngAfterContentInit(): void {
+    console.log("content text paragrph:" + this.paragraph.nativeElement.textContent);
   }
 
 }
